04255eca95b14f9c33ce498b6588bfbeb076693a
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-07 21:39:44 +0545

    Added people list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,7 +3,26 @@ interface IProps {
 }
 
 const List = ({ people }: IProps) => {
-	return <div>Imma list</div>;
+	const renderList = (): JSX.Element[] => {
+		return people.map((person) => {
+			return (
+				<li className='List'>
+					<div className='List-header'>
+						<img
+							className='List-img'
+							src={person.url}
+							alt='Profile-pic'
+						/>
+						<h2>{person.name}</h2>
+					</div>
+					<p>{person.age} years old</p>
+					<p className='List-note'>{person.note}</p>
+				</li>
+			);
+		});
+	};
+
+	return <ul>{renderList()}</ul>;
 };
 
 export default List;
